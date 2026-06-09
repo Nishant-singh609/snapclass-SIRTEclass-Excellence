72bd83282532863c7430bea258c8aa4dee08053a
NISHANT439 <luckeysingh78151@gmail.com>
2026-06-09 22:22:10 +0530

    Update secrets and share subject dialog

--- src/component/dialog_share_subject.py
+++ src/component/dialog_share_subject.py
@@ -7,7 +7,7 @@ import io
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
 
-    app_domain = "snapclass-main.streamlit.app"
+    app_domain = "snapclass-sirteclass-excellence-espvyvjqkfcwqvnw5ssr6m.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
     st.header("Scan to Join")
     qr = segno.make(join_url)
